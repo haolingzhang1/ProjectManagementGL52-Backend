18f388457773008fc10607ad1358e6411a0ed640
haoling <haolingzhang@outlook.com>
2021-05-07 23:53:20 +0800

    initialize

---
+++ src/main/java/fr/utbm/gl52/Gl52Application.java
@@ -0,0 +1,13 @@
+package fr.utbm.gl52;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Gl52Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Gl52Application.class, args);
+	}
+
+}
---
+++ src/test/java/fr/utbm/gl52/Gl52ApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.utbm.gl52;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Gl52ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
